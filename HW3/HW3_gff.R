e262e038783f30fe04d32640304ec456b27f2f8b
isa-caste <isabella.castellano143@gmail.com>
2026-03-09 15:21:55 -0400

    Updated R code files"

--- HW3/HW3_gff.R
+++ HW3/HW3_gff.R
@@ -1,6 +1,44 @@
-## PART 2: Predict the operons in a Crop microbiome from Hoatzin
-# load in file & add headers
-metagenome <- read.delim("metagenome.gff", header = TRUE, sep = "\t")
-colnames(metagenome) <- c("seqid", "source", "type", "start", "end", "score", "strand", "phase", "attributes")
-# View(metagenome)
+###### PART 2: Predict the operons in a Crop microbiome ######
 
+# load library
+library(dplyr)
+library(stringr)
+# load in file & add headers
+setwd("C:/Users/icwin/OneDrive - Indiana University/Documents/INFO-B-528/HW3")
+metagenome <- read.delim("metagenome.gff", header = TRUE, sep = "\t")
+colnames(metagenome) <- c("Seq_ID", "Source", "Type", "Start", "End", "Score", "Strand", "Phase", "Attributes")
+
+# create function for  detecting operons
+operon_pred <- function(genome, filename) {
+  # Parse Attributes column to extract locus_tag and product
+  genome <- genome %>%
+    mutate(
+      Gene = str_extract(Attributes, "locus_tag=([^;]+)", group = 1),
+      Product = str_extract(Attributes, "product=(.+)$", group = 1),
+      Location = paste0(Start, "..", End)
+    ) %>%
+    select(Start, End, Strand, Gene, Product) 
+  # compare adjacent genes 
+  genome_with_operons <- genome %>%
+    arrange(Start) %>%
+    mutate(
+      Start = as.numeric(Start),
+      End = as.numeric(End),
+      next_Start = lead(Start),
+      next_Strand = lead(Strand),
+      same_operon = (Strand == next_Strand) & ((next_Start - End) < 50),
+      same_operon = ifelse(is.na(same_operon), FALSE, same_operon),
+      operon_group = cumsum(!same_operon),
+      Operon_ID = dense_rank(operon_group)
+    ) %>%
+    select(Operon_ID, Gene, Product)
+  num_operons <- max(genome_with_operons$Operon_ID)
+  # save results as csv
+  write.csv(genome_with_operons, paste0(filename, "_operons.csv"), row.names = FALSE)
+  print(paste("Number of operons found in", filename, ":", num_operons))
+  # return the results
+  return(genome_with_operons)
+}
+
+# run function for given file
+operon_pred(metagenome, "Metagenome")
--- HW3/HW3_ppt.R
+++ HW3/HW3_ppt.R
@@ -1,19 +1,49 @@
-## PART 1: Predict operons using ptt files for all 4 genomes
+###### PART 1: Predict operons using ptt files for 4 genomes ######
+
+# load library
+library(dplyr)
 # set working directory & load in files
 setwd("C:/Users/icwin/OneDrive - Indiana University/Documents/INFO-B-528/HW3")
 e_coli <- read.table("E_coli.ptt", header = TRUE, sep = "\t")
 b_subtilis <- read.delim("B_subtilis.ptt", header = TRUE, sep = "\t")
 halobac <- read.delim("Halobacterium.ptt", header = TRUE, sep = "\t")
 synechoc <- read.delim("Synechocystis.ptt", header = TRUE, sep = "\t")
-# View(e_coli)
-# View(b_subtilis)
-# View(halobac)
-# View(synechoc)
 
-#######################################################
-# Operons:  groups of genes that work together as a unit.
-# genes belong in the same operon if they meet these criteria:
-# They're adjacent to each other (next to one another on the genome)
-# They're co-directional (pointing in the same direction — both reading left-to-right or both right-to-left)
-# The gap between them is less than 50 base pairs
-#################################################################
+# create function for detecting operons
+operon_pred <- function(genome, filename) {
+  # split location column to get start & end position for gene
+  split_data <- strsplit(genome$Location, "\\.\\.")
+  matrix_data <- do.call(rbind, split_data)
+  genome$Start <- matrix_data[,1]
+  genome$End <- matrix_data[,2]
+  # drop location column
+  genome$Location <- NULL
+  # select necessary columns for prediction
+  genome <- genome[, c("Start", "End", "Strand", "Gene", "Product")]
+  # compare adjacent genes 
+  genome_with_operons <- genome %>%
+    arrange(Start) %>%
+    mutate(
+      Start = as.numeric(Start),
+      End = as.numeric(End),
+      next_Start = lead(Start),
+      next_Strand = lead(Strand),
+      same_operon = (Strand == next_Strand) & ((next_Start - End) < 50),
+      same_operon = ifelse(is.na(same_operon), FALSE, same_operon),
+      operon_group = cumsum(!same_operon),
+      Operon_ID = dense_rank(operon_group)
+    ) %>%
+    select(Operon_ID, Gene, Product)
+  num_operons <- max(genome_with_operons$Operon_ID)
+  # save results as csv
+  write.csv(genome_with_operons, paste0(filename, "_operons.csv"), row.names = FALSE)
+  print(paste("Number of operons found in", filename, ":", num_operons))
+  # return the results
+  return(genome_with_operons)
+}
+
+# run function for all 4 files
+operon_pred(e_coli, "E_coli")
+operon_pred(b_subtilis, "B_subtilis")
+operon_pred(halobac, "Halobacterium")
+operon_pred(synechoc, "Synechocystis")
