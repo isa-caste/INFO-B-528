3eef5b030758d016a1d5b3762a3352d5e4e3fa53
isa-caste <isabella.castellano143@gmail.com>
2026-03-30 18:41:44 -0400

    add hw 5 files

---
+++ HW5/HW5.R
@@ -0,0 +1,14 @@
+assignment 5
+given 2 protein lists 1 & 2
+
+Prt 1
+- use libraries for computing degree
+- compute avg closeting coeff
+- see if its scale free
+- if # nodes with given degree vs degree itself has a slope of gamma where gamma is between 2-3
+- do log and log on both lines and should be a straight line 
+^ this is for whole network
+
+Prt 2
+- shortest path length btwn all nodes
+^ for protein lists
