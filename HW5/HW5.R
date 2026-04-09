3b5a99bfe8d24b458d523c59e0f9dd2f1aa4bdc3
isa-caste <isabella.castellano143@gmail.com>
2026-04-09 11:15:01 -0400

    Code update

--- HW5/HW5.R
+++ HW5/HW5.R
@@ -1,7 +1,4 @@
-assignment 5
-given 2 protein lists 1 & 2
-
-Prt 1
+##### Part 1 #####
 - use libraries for computing degree
 - compute avg closeting coeff
 - see if its scale free
@@ -9,6 +6,14 @@ Prt 1
 - do log and log on both lines and should be a straight line 
 ^ this is for whole network
 
-Prt 2
+setwd("C:/Users/icwin/OneDrive - Indiana University/Documents/INFO-B-528/HW5")
+prot_list_1 <- read.table("protein-list1.txt")
+prot_list_2 <- read.table("protein-list2.txt")
+human_ppi <- read.table("Human-PPI.txt", header = TRUE)
+View(prot_list_2)
+View(prot_list_1)
+View(human_ppi)
+
+##### Part 2 #####
 - shortest path length btwn all nodes
 ^ for protein lists
